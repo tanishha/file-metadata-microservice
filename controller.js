258a0864cf6674dc9f0321dd72d16bd2d86551b2
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-13 23:46:31 +0545

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,20 +1,10 @@
 function postFile(req, res) {
-
-    // const data = req.body;
-    // if (req.files) {
-    //     data.images = req.files.map(item => { //images ma all array but filename matra
-    //         return item.filename
-    //     })
-    // }
-        // const mimeType = req.file.mimetype.split('/')[0];
-
-        res.json({
+res.json({
             name:req.file.originalname,
             type:req.file. mimetype,
             size:req.file.size
         }) 
-        console.log(req.file)
-    }
+}
 
 module.exports = {
     postFile
